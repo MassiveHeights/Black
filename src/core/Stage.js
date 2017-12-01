7a8e87f7a51f12b64994b30b0a00492acb19639b
ddrymalyk <commics@gmail.com>
2017-12-01 12:50:11 +0200

    * merged Stage and MRComponent

--- src/core/Stage.js
+++ src/core/Stage.js
@@ -11,6 +11,31 @@ class Stage extends GameObject {
 
     // set default
     // this.scaleMode = StageScaleMode.NORMAL;
+    this.mWidth = 960;
+    this.mHeight = 640;
+
+    this.mStageWidth = 0;
+    this.mStageHeight = 0;
+    this.mStageScaleFactor = 0;
+  }
+
+  __refresh( ){
+    let size = Black.instance.viewport.size;
+    let windowWidth = size.width;
+    let windowHeight = size.height;
+    let mw = this.LP(windowWidth * this.mHeight / windowHeight, windowWidth * this.mWidth / windowHeight);
+    let mh = this.LP(windowHeight * this.mWidth / windowWidth, windowHeight * this.mHeight / windowWidth);
+    let scaleFactor = Math.max(mw / windowWidth, mh / windowHeight);
+    let width = windowWidth * scaleFactor;
+    let height = windowHeight * scaleFactor;
+
+    this.mStageScaleFactor = Math.min(windowWidth / width, windowHeight / height);
+    this.mStageWidth = width;
+    this.mStageHeight = height;
+  }
+
+  LP(land, port) {
+    return this.isLandscape ? land : port;
   }
 
   get scaleMode() {
@@ -19,7 +44,14 @@ class Stage extends GameObject {
 
   set scaleMode(value) {
     this.mScaleMode = value;
-    this.mScaleX = this.mScaleY = 1 / this.scaleFactor;
+    
+    if (this.mScaleMode === StageScaleMode.FIXED) {
+      this.__refresh();
+      this.mScaleX = this.mScaleY = this.mStageScaleFactor;
+    } else {
+      this.mScaleX = this.mScaleY = 1 / this.scaleFactor;
+    }
+
     this.setTransformDirty();
   }
 
@@ -28,11 +60,28 @@ class Stage extends GameObject {
   }
 
   get width() {
+    if (this.mScaleMode === StageScaleMode.FIXED) {
+     return this.mStageWidth;
+    }
+
     return Black.instance.viewport.size.width * this.scaleFactor;
   }
 
   get height() {
+    if (this.mScaleMode === StageScaleMode.FIXED) {
+      return this.mStageHeight;
+    }
+
     return Black.instance.viewport.size.height * this.scaleFactor;
+  }
+
+  get isLandscape() {
+    let size = Black.instance.viewport.size;
+    return size.width >= size.height;
+  }
+
+  get isPortrait() {
+    return !this.isLandscape;
   }
 
   getBounds(space = undefined, includeChildren = true, outRect = undefined) {
@@ -57,12 +106,12 @@ class Stage extends GameObject {
   set x(value) { Debug.error('Not allowed.'); }
   set y(value) { Debug.error('Not allowed.'); }
 
-  set rotation(value) { Debug.error('Not allowed.'); }  
+  set rotation(value) { Debug.error('Not allowed.'); }
 
   set width(value) { Debug.error('Not allowed.'); }
-  set height(value) { Debug.error('Not allowed.'); }  
+  set height(value) { Debug.error('Not allowed.'); }
 
-  set name(value) { Debug.error('Not allowed.'); }  
+  set name(value) { Debug.error('Not allowed.'); }
 }
 
 /**
@@ -73,5 +122,6 @@ class Stage extends GameObject {
 /* @echo EXPORT */
 var StageScaleMode = {
   NORMAL: 'normal',
-  NO_SCALE: 'noScale'
+  NO_SCALE: 'noScale',
+  FIXED: 'fixed'
 };
