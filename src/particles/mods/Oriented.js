e4c87d2625a988fdbb8641c9992546b7580c5c4e
ddrymalyk <commics@gmail.com>
2018-03-23 19:07:25 +0200

    Added optional angle shift

--- src/particles/mods/Oriented.js
+++ src/particles/mods/Oriented.js
@@ -9,14 +9,15 @@ class Oriented extends Modifier {
   /**
    * Creates new instance of oriented modifier.
    */
-  constructor() {
+  constructor(angleShift = 0) {
     super(false);
+    this.mAngleShift = angleShift;
   }
 
   /**
    * @inheritDoc
    */
   update(emitter, particle, dt) {
-    particle.r = (Math.atan2(particle.vy, particle.vx) * MathEx.RAD2DEG - 90) * dt;
+    particle.r = (Math.atan2(particle.vy, particle.vx) * MathEx.RAD2DEG - (90 + this.mAngleShift)) * dt;
   }
 }
