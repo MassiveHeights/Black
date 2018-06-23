1e5bdd4d592cece8f042ac24b82fdc880632a5b6
62316e <62316e@gmail.com>
2018-06-23 14:12:05 +0300

    Fixed TextRenderer incorrect transform Changed order of onRender call for Component Fixed broken tween

--- src/drivers/canvas/CanvasDriver.js
+++ src/drivers/canvas/CanvasDriver.js
@@ -101,15 +101,16 @@ class CanvasDriver extends VideoNullDriver {
     let skipChildren = false;
     let renderer = /** @type {DisplayObject} */ (child).mRenderer;
 
+    for (let i = 0; i < child.mComponents.length; i++) {
+      const comp = child.mComponents[i];
+      comp.onRender();
+    }
+
     if (renderer != null) {
       renderer.parent = parentRenderer;
-      renderer.preRender(this, session);
+      renderer.preRender(this, session);      
 
       /** @type {DisplayObject} */ (child).onRender();
-      for (let i = 0; i < child.mComponents.length; i++) {
-        const comp = child.mComponents[i];
-        comp.onRender();
-      }
 
       renderer.begin(this, session);
 
