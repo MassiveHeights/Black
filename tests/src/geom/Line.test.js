e6bdf071f4c317cb36c6b6d9f150bd5939b83240
62316e <62316e@gmail.com>
2018-06-25 17:27:53 +0300

    Geom tests

--- tests/src/geom/Line.test.js
+++ tests/src/geom/Line.test.js
@@ -1,116 +1,122 @@
+import { Line, Circle, Vector } from './../../../dist/black-es6-module'
+
 describe('Line', function () {
   it('should initialize new empty Line', function () {
     var l = new Line(new Vector(), new Vector());
-    assert.equal(l.start.x, 0);
-    assert.equal(l.end.y, 0);
-    assert.equal(l.start.x, 0);
-    assert.equal(l.end.y, 0);
+    expect(l.start.x).toBe(0);
+    expect(l.end.y).toBe(0);
+    expect(l.start.x).toBe(0);
+    expect(l.end.y).toBe(0);
   });
 
   it('should intersect line', function () {
     var l1 = new Line(new Vector(0, 0), new Vector(1, 1));
     var l2 = new Line(new Vector(0.9, 1), new Vector(2, 0));
 
-    assert.isOk(l1.intersects(l2));
+    expect(l1.intersects(l2)).toBe(true);
   });
 
   it('should not intersect line', function () {
     var l1 = new Line(new Vector(0, 0), new Vector(1, 1));
     var l2 = new Line(new Vector(1, 1), new Vector(2, 0));
 
-    assert.isNotOk(l1.intersects(l2));
+    expect(l1.intersects(l2)).not.toBe(true);
   });
 
   it('should intersect circle', function () {
     var l = new Line(new Vector(0, 0), new Vector(1, 1));
     var c = new Circle(2, 1, 1);
 
-    assert.isOk(l.intersectsCircle(c));
+    expect(l.intersectsCircle(c)).toBe(true);
   });
 
   it('should not intersect circle', function () {
     var l = new Line(new Vector(0, 0), new Vector(1, 1));
     var c = new Circle(2.01, 2, 1);
 
-    assert.isNotOk(l.intersectsCircle(c));
+    expect(l.intersectsCircle(c)).not.toBe(true);
   });
 
   it('should clone line', function () {
     var l1 = new Line(new Vector(1, 1), new Vector(2, 2));
     var l2 = l1.clone();
 
-    expect(l1).to.eql(l2);
-    expect(l1).to.not.equal(l2);
+    expect(l1.equals(l2)).toBe(true);
+    expect(l1).not.toBe(l2);
   });
 
   it('should set new line properties', function () {
     var l = new Line(new Vector(1, 1), new Vector(2, 2));
     l.set(new Vector(3, 3), new Vector(4, 5));
 
-    assert.equal(l.start.x, 3);
-    assert.equal(l.start.y, 3);
-    assert.equal(l.end.x, 4);
-    assert.equal(l.end.y, 5);
+    expect(l.start.x).toBe(3);
+    expect(l.start.y).toBe(3);
+    expect(l.end.x).toBe(4);
+    expect(l.end.y).toBe(5);
   });
 
   it('should copy line properties to another', function () {
     var l1 = new Line(new Vector(1, 1), new Vector(2, 2));
     var l2 = new Line(new Vector(), new Vector());
 
-    expect(l1).to.eql(l1.copyTo(l2));
-    expect(l1).to.not.equal(l2);
+    l1.copyTo(l2);
+
+    expect(l1.equals(l2)).toBe(true);
+    expect(l1).not.toBe(l2);
   });
 
   it('should copy another line properties to this', function () {
     var l1 = new Line(new Vector(1, 1), new Vector(2, 2));
     var l2 = new Line(new Vector(3, 3), new Vector(4, 4));
 
-    expect(l2).to.eql(l1.copyFrom(l2));
-    expect(l2).to.not.equal(l1);
+    l1.copyFrom(l2);
+
+    expect(l1.equals(l2)).toBe(true);
+    expect(l2).not.toBe(l1);
   });
 
   it('should represent line object as string', function () {
     var l1 = new Line(new Vector(1.55555, 1), new Vector(2, 2));
 
-    assert.equal(l1.toString(3), 'Line { start: Vector: { x: 1.556, y: 1.000 }, end: Vector: { x: 2.000, y: 2.000 } }');
+    expect(l1.toString(3)).toBe('Line { start: Vector: { x: 1.556, y: 1.000 }, end: Vector: { x: 2.000, y: 2.000 } }');
   });
 
   it('should be equals', function () {
     var l1 = new Line(new Vector(1, 1), new Vector(2, 2));
     var l2 = new Line(new Vector(1, 1), new Vector(2, 2.09));
 
-    assert.isOk(l1.equals(l2, 0.1));
+    expect(l1.equals(l2, 0.1)).toBe(true);
   });
 
   it('should not be equals', function () {
     var l1 = new Line(new Vector(1, 1), new Vector(2, 2));
     var l2 = new Line(new Vector(1, 1), new Vector(2, 2.1));
 
-    assert.isNotOk(l1.equals(l2));
+    expect(l1.equals(l2)).not.toBe(true);
   });
 
   it('should contain point XY', function () {
     var l = new Line(new Vector(1, 1), new Vector(2, 2));
 
-    assert.isOk(l.containsXY(1.5, 1.5));
+    expect(l.containsXY(1.5, 1.5)).toBe(true);
   });
 
   it('should not contain point XY', function () {
     var l = new Line(new Vector(1, 1), new Vector(2, 2));
 
-    assert.isNotOk(l.containsXY(1.51, 1.5));
+    expect(l.containsXY(1.51, 1.5)).not.toBe(true);
   });
 
   it('should contain point', function () {
     var l = new Line(new Vector(1, 1), new Vector(2, 2));
 
-    assert.isOk(l.contains(new Vector(1.5, 1.5)));
+    expect(l.contains(new Vector(1.5, 1.5))).toBe(true);
   });
 
   it('should not contain point', function () {
     var l = new Line(new Vector(1, 1), new Vector(2, 2));
 
-    assert.isNotOk(l.contains(new Vector(1.51, 1.5)));
+    expect(l.contains(new Vector(1.51, 1.5))).not.toBe(true);
   });
 
   it('should reverse line', function () {
@@ -119,33 +125,35 @@ describe('Line', function () {
     var start = clone.start;
     var end = clone.end;
     l.reverse();
-    expect(start).to.eql(l.end);
-    expect(end).to.eql(l.start);
-    expect(start).to.not.equal(l.end);
-    expect(end).to.not.equal(l.start);
+
+    expect(start.equals(l.end)).toBe(true);
+    expect(end.equals(l.start)).toBe(true);
+
+    expect(start).not.toBe(l.end);
+    expect(end).not.toBe(l.start);
   });
 
   it('should normalize line', function () {
     var l = new Line(new Vector(1, 1), new Vector(1, 4));
     l.normalize();
-    expect(l).to.eql(new Line(new Vector(1, 1), new Vector(1, 2)));
+    expect(l).toEqual(new Line(new Vector(1, 1), new Vector(1, 2)));
   });
 
   it('should scale line', function () {
     var l = new Line(new Vector(1, 1), new Vector(1, 2));
     l.scale(3);
-    expect(l).to.eql(new Line(new Vector(1, 1), new Vector(1, 4)));
+    expect(l).toEqual(new Line(new Vector(1, 1), new Vector(1, 4)));
   });
 
   it('should calc line length', function () {
     var l = new Line(new Vector(1, 1), new Vector(1, 3));
-    assert.equal(l.length(), 2);
+    expect(l.length()).toBe(2);
   });
 
   it('should calc center', function () {
     var l = new Line(new Vector(1, 1), new Vector(1, 3));
     var res = new Vector();
     l.center(res);
-    expect(res).to.eql(new Vector(1, 2));
+    expect(res).toEqual(new Vector(1, 2));
   });
 });
--- tests/src/geom/Polygon.test.js
+++ tests/src/geom/Polygon.test.js
@@ -9,12 +9,12 @@ describe('Polygon', function () {
   test('should create polygon from path', function () {
     var p = Polygon.fromPath('0 0 1 0 1 1');
 
-    expect(p.vertices[0].x).toEqual(0);
-    expect(p.vertices[0].y).toEqual(0);
-    expect(p.vertices[1].x).toEqual(1);
-    expect(p.vertices[1].y).toEqual(0);
-    expect(p.vertices[2].x).toEqual(1);
-    expect(p.vertices[2].y).toEqual(1);
+    expect(p.vertices[0].x).toBe(0);
+    expect(p.vertices[0].y).toBe(0);
+    expect(p.vertices[1].x).toBe(1);
+    expect(p.vertices[1].y).toBe(0);
+    expect(p.vertices[2].x).toBe(1);
+    expect(p.vertices[2].y).toBe(1);
   });
 
   test('should rotate polygon around center', function () {
@@ -42,78 +42,78 @@ describe('Polygon', function () {
     p.setTranslation(new Vector(1, 1));
 
     expect(p.vertices).toEqual(Polygon.fromPath('0 1 1 2 2 1 1 0').vertices);
-    expect(p.center.equals(new Vector(1, 1))).toEqual(true);
+    expect(p.center.equals(new Vector(1, 1))).toBe(true);
   });
 
   test('should collide with another polygon', function () {
     var p1 = Polygon.fromPath('0 0 1 1 1 0');
     var p2 = Polygon.fromPath('0 0 0 1 1 0');
-    expect(p1.collide(p2)).toEqual(true);
+    expect(p1.collide(p2)).toBe(true);
   });
 
   test('should not collide with another polygon', function () {
     var p1 = Polygon.fromPath('0 0 1 1 0 1');
     var p2 = Polygon.fromPath('1 0 2 1 1.05 1');
-    expect(p1.collide(p2)).not.toEqual(true);
+    expect(p1.collide(p2)).not.toBe(true);
   });
 
   test('should collide with circle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var c = new Circle(0, 1, 0.8);
-    expect(p.collideCircle(c)).toEqual(true);
+    expect(p.collideCircle(c)).toBe(true);
   });
 
   test('should not collide with circle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var c = new Circle(0, 1, 0.7);
-    expect(p.collideCircle(c)).not.toEqual(true);
+    expect(p.collideCircle(c)).not.toBe(true);
   });
 
   test('should collide with rectangle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var r = new Rectangle(0, 0, 0.1, 1);
-    expect(p.collideRectangle(r)).toEqual(true);
+    expect(p.collideRectangle(r)).toBe(true);
   });
 
   test('should not collide with rectangle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var r = new Rectangle(-0.1, 0, 0.1, 1);
-    expect(p.collideRectangle(r)).not.toEqual(true);
+    expect(p.collideRectangle(r)).not.toBe(true);
   });
 
   test('should overlap another polygon', function () {
     var p1 = Polygon.fromPath('0 0 1 1 1 0');
     var p2 = Polygon.fromPath('0 0 1 1 1 0');
-    expect(p1.overlap(p2)).toEqual(true);
+    expect(p1.overlap(p2)).toBe(true);
   });
 
   test('should not overlap another polygon', function () {
     var p1 = Polygon.fromPath('0 0 1 1 1 0');
     var p2 = Polygon.fromPath('0 0 1 1 1 -0.1');
-    expect(p1.overlap(p2)).not.toEqual(true);
+    expect(p1.overlap(p2)).not.toBe(true);
   });
 
   test('should overlap circle', function () {
     var p = Polygon.fromPath('0 0 1 1 2 0 1 -1');
     var c = new Circle(1, 0, 0.7);
-    expect(p.overlapCircle(c)).toEqual(true);
+    expect(p.overlapCircle(c)).toBe(true);
   });
 
   test('should not overlap circle', function () {
     var p = Polygon.fromPath('0 0 1 1 2 0 1 -1');
     var c = new Circle(1, 0, 0.8);
-    expect(p.overlapCircle(c)).not.toEqual(true);
+    expect(p.overlapCircle(c)).not.toBe(true);
   });
 
   test('should overlap rectangle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var r = new Rectangle(0.5, 0, 0.5, 0.5);
-    expect(p.overlapRectangle(r)).toEqual(true);
+    expect(p.overlapRectangle(r)).toBe(true);
   });
 
   test('should not overlap rectangle', function () {
     var p = Polygon.fromPath('0 0 1 1 1 0');
     var r = new Rectangle(0.51, 0, 0.5, 0.5);
-    expect(p.overlapRectangle(r)).not.toEqual(true);
+    expect(p.overlapRectangle(r)).not.toBe(true);
   });
 });
