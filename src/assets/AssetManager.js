2845dbe6b30b92946cf286ea0be723eae9a7dd02
62316e <62316e@gmail.com>
2018-07-16 17:38:27 +0300

    Added path getter

--- src/assets/AssetManager.js
+++ src/assets/AssetManager.js
@@ -510,6 +510,17 @@ class AssetManager extends MessageDispatcher {
   get state() {
     return this.mState;
   }
+
+  /**
+   * Always returns 'AssetManager', can be used to overhear AssetManager's messages.
+   *
+   * @override
+   * @readonly
+   * @return {string|null}
+   */
+  get path() {
+    return 'AssetManager';
+  }
 }
 
 /**
